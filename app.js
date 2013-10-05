6241e5838241231b44736434eb294850b1016a86
judyhe <judy.w.he@gmail.com>
2013-10-05 09:53:26 -0400

    setup stuff

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var express = require("express");
+require('./lib/env');
+
+var express = require('express');
 var app = express();
-app.use(express.logger());
+app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.use(express.static(__dirname + '/public', { maxAge: 31557600000 }));
+app.use(express.logger({ format: 'dev' }));
+app.use(express.bodyParser());
+app.use(app.router);
+
+var Game = require('./models/game');
+
+app.get('/', function(req, res){
+  res.render('index');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+var addressparser = require("addressparser");
+
+app.post('/email', function(req, res){
+  var subject = req.body.subject.toLowerCase().trim();
+
+  res.send(200);
 });
+
+app.listen(process.env.PORT || 3000);
---
+++ models/game.js
@@ -0,0 +1,3 @@
+var Game = require('./../lib/model')('games');
+
+module.exports = Game;
